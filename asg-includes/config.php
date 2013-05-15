558d7b64c01b15350073c771ff06a4cfc7928d05
Stefan Cosma <stefcosma@gmail.com>
2013-05-15 22:53:41 +0300

    Update config.php

--- asg-includes/config.php
+++ asg-includes/config.php
@@ -22,10 +22,10 @@
         define('FUNCTIONS', 'functions/');
         
     // Database server credentials
-    define('DB_SERVER','mysql0.db.koding.com');
-    define('DB_USER','stefanbc_43f2da4');
-    define('DB_PASS','6f9c99f4346e41c6925b29e06573fd51a0f8ac13');
-    define('DATABASE','stefanbc_43f2da4');
+    define('DB_SERVER','DB_SERVER');
+    define('DB_USER','DB_USER');
+    define('DB_PASS','DB_PASS');
+    define('DATABASE','DB_NAME');
     
     // You should change this on each install! Call ?salt=true to generate a new AUTH_SALT
     define('AUTH_SALT','2*W1NJd~Q6$&Pj2L7(-,xi.L@CCr=ns<|=QGX|Fu1lqsj5$^pf.MH1/z><,;ooeu');
