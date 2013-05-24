02ee99000b28701e6faf7dcb5fba7457f7db6d04
Stefan Cosma <stefcosma@gmail.com>
2013-05-24 21:56:52 +0300

    Update settings.php

--- asg-includes/settings.php
+++ asg-includes/settings.php
@@ -2,15 +2,15 @@
 	/*
     * The basecamp for our app
     */
-    define('HTTP', 'http://stefanbc.koding.com/asgar');
+    define('HTTP', '');
 
     /*
     * The database configuration settings
     */
-    define('DB_SERVER','mysql0.db.koding.com');
-    define('DB_USER','stefanbc_43f2da4');
-    define('DB_PASS','6f9c99f4346e41c6925b29e06573fd51a0f8ac13');
-    define('DATABASE','stefanbc_43f2da4');
+    define('DB_SERVER','');
+    define('DB_USER','');
+    define('DB_PASS','');
+    define('DATABASE','');
     
     /*
     * You should change this on each install! Call ?salt=true to generate a new AUTH_SALT
