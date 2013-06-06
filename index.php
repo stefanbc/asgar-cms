3dd9ebd3d0ae76fbcf8897931fd30a24603558f6
stefanbc <stefcosma@gmail.com>
2013-06-06 09:51:46 +0300

    Removed unwanted spaces from main index file

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-<?php
-    /**
-     * Front to the Asgar CMS Platform. This file doesn't do anything, 
-     * it only tells Asgar to start the platform.
-     *
-     * @package Asgar
-     */
-    
-    // Start Asgar
-    require('asg-includes/asg_start.php');
-
-        
-    // Stop Asgar
-    require(INCLUDES . 'asg_stop.php');
+<?php
+    /**
+     * Front to the Asgar CMS Platform. This file doesn't do anything, 
+     * it only tells Asgar to start the platform.
+     *
+     * @package Asgar
+     */
+
+    // Start Asgar
+    require('asg-includes/asg_start.php');
+
+    // Stop Asgar
+    require(INCLUDES . 'asg_stop.php');
 ?>
