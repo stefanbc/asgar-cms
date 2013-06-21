488e38de6d6f15d221b2af95aef7612626aa7cb7
stefanbc <stefanbc@vm-0.stefanbc.koding.kd.io>
2013-06-21 17:06:20 +0000

    Removed dev line from htaccess

--- asg-includes/settings.php
+++ asg-includes/settings.php
@@ -2,15 +2,15 @@
 	/*
     * The basecamp for our app
     */
-    define('HTTP', '');
+    define('HTTP', 'http://stefanbc.kd.io/asgar');
 
     /*
     * The database configuration settings
     */
-    define('DB_SERVER','');
-    define('DB_USER','');
-    define('DB_PASS','');
-    define('DATABASE','');
+    define('DB_SERVER','localhost');
+    define('DB_USER','stefanbc');
+    define('DB_PASS','f4vTewnFHZVhA4qZ');
+    define('DATABASE','stefanbc_asgar');
     
     /*
     * You should change this on each install! Call ?salt to generate a new AUTH_SALT
