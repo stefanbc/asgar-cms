b037ea6d652e1078b5ffd06a86ff019f613097c4
stefanbc <stefcosma@gmail.com>
2013-06-25 13:35:32 +0000

    Updated admin actions on posts listings

--- asg-includes/admin/actions/edit_post.php
+++ asg-includes/admin/actions/edit_post.php
@@ -1,1 +1,19 @@
-<h1>Edit Post</h1>
+<?
+    echo '<h1>Edit Post</h1>';
+    
+    $post_query = asg_db_query("select * from " . TABLE_POSTS . " where ID = '" . $_REQUEST['post_id'] . "'");
+    
+    if(!empty($post_query)){
+        foreach($post_query as $post){
+            echo $post['post_title'] . "\n\r";
+            echo $post['post_author'] . "\n\r";
+            echo $post['post_content'] . "\n\r";
+            echo $post['post_status'] . "\n\r";
+            echo $post['post_date'] . "\n\r";
+            echo $post['comment_status'] . "\n\r";
+        }
+    } else {
+        echo 'The post has not been found!';
+    }
+    
+?>
