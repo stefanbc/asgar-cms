3605220242438805ed1f2c8d4af1d1f2a2cdf8aa
stefanbc <stefcosma@gmail.com>
2013-06-25 14:36:35 +0300

    Updated UI on default theme

--- asg-includes/admin/actions/new_page.php
+++ asg-includes/admin/actions/new_page.php
@@ -1,2 +1,12 @@
 <h1>New Page</h1>
 <textarea id="editor" name="content"></textarea>
+
+<?
+	$page_title = '';
+	$page_theme = '';
+	$page_content = '';
+	$page_status = '';
+	$page_parent = '';
+	$page_order = '';
+
+?>
--- asg-includes/admin/actions/new_post.php
+++ asg-includes/admin/actions/new_post.php
@@ -1,1 +1,13 @@
-<h1>New Post</h1>
+<?
+
+	echo '<h1>New Post</h1>';
+
+	$post_title = '';
+	$post_content = '';
+	$post_status = '';
+	$post_comments_status = '';
+	$post_type = '';
+	$post_date = '';
+
+	echo '<textarea id="editor" name="post_content"></textarea>';
+?>
